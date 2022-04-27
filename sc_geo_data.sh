9c4b7f83bff526f904e337a0785d94c0699ea625
Jamey Hopkins <35248862+CipherMethod@users.noreply.github.com>
2022-04-26 21:16:01 -0700

    Update sc_geo_data.sh
    provide example usage/output

--- sc_geo_data.sh
+++ sc_geo_data.sh
@@ -2,6 +2,14 @@
 #            Show Geo Data of Connected Shoutcast Connections
 
 # 20220128 - jamey hopkins
+#
+# $ ./sc_geo_data.sh | grep city
+# city: London, region: England, country: GB, timezone: Europe/London,
+# city: Phoenix, region: Arizona, country: US, timezone: America/Phoenix,
+# city: Borken, region: North Rhine-Westphalia, country: DE, timezone: Europe/Berlin,
+# city: Falkenstein, region: Saxony, country: DE, timezone: Europe/Berlin,
+# city: Des Moines, region: Washington, country: US, timezone: America/Los_Angeles,
+# city: Iztapalapa, region: Mexico City, country: MX, timezone: America/Mexico_City,
 
 PORT=8000 # sc default port
 
