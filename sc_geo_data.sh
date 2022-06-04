a688e2aeeb0d7406a5c58360f8aa529fd5a03395
Jamey Hopkins <35248862+CipherMethod@users.noreply.github.com>
2022-06-03 20:06:04 -0700

    Merge source info to single line. Get rid of spacing between lines.

--- sc_geo_data.sh
+++ sc_geo_data.sh
@@ -2,21 +2,13 @@
 #            Show Geo Data of Connected Shoutcast Connections
 
 # 20220128 - jamey hopkins
-#
-# $ ./sc_geo_data.sh | grep city
-# city: London, region: England, country: GB, timezone: Europe/London,
-# city: Phoenix, region: Arizona, country: US, timezone: America/Phoenix,
-# city: Borken, region: North Rhine-Westphalia, country: DE, timezone: Europe/Berlin,
-# city: Falkenstein, region: Saxony, country: DE, timezone: Europe/Berlin,
-# city: Des Moines, region: Washington, country: US, timezone: America/Los_Angeles,
-# city: Iztapalapa, region: Mexico City, country: MX, timezone: America/Mexico_City,
+# 20220603 jah - merge source info to single line of output.  get rid of spacing between lines.
 
 PORT=8000 # sc default port
 
 SERVER=`netstat -nputw 2>/dev/null | grep $PORT | grep ESTABLISHED | awk '{ print $5 }' | cut -f1 -d: | grep -v 127.0.0.1  | sort -n | xargs echo `
 for S in `echo $SERVER`; do
-  echo $S
+  echo -n "$S\t"
   curl -s https://ipinfo.io/$S | grep "city\|region\|country\|timezone" | xargs echo
-  echo
 done
 
